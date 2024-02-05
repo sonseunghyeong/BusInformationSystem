3e1f666c5791117472f94880ddf9ceb2da451e7f
sonseunghyeong <116471675+sonseunghyeong@users.noreply.github.com>
2024-02-05 15:58:53 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri("C:\\Users\\Administrator\\source\\repos\\WpfApp2\\WpfApp2/main_bk.png"));
+
+        }
+        
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
